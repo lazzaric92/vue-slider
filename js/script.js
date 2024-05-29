b7a50ef1367d324353f47f7dd49e0d20ec3e1758
lazzaric92 <lazzaric_92@hotmail.it>
2024-05-29 12:22:39 +0200

    added vue in js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,44 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            slides: [
+                {
+                    image: 'img/01.webp',
+                    title: 'Marvel\'s Spiderman Miles Morale',
+                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                },
+                {
+                    image: 'img/02.webp',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                },
+                {
+                    image: 'img/03.webp',
+                    title: 'Fortnite',
+                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                },
+                {
+                    image: 'img/04.webp',
+                    title: 'Stray',
+                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                },
+                {
+                    image: 'img/05.webp',
+                    title: "Marvel's Avengers",
+                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                }
+                ]
+        }
+        
+    }
+
+}).mount('#app')
+
+
+// || MEMO
+/*  slides array
 const slides = [
 {
     image: 'img/01.webp',
@@ -23,3 +64,21 @@ const slides = [
 ];
 
 console.log(slides);
+*/
+
+/* div.container content
+<div class="slider-wrapper" tabindex="0">
+                <div class="item">
+                    <img src="img/01.webp" alt="Spiderman" />
+                    <div class="text">
+                        <h3>Spiderman</h3>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit
+                            tempore aliquid deleniti aut
+                            veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum
+                            corporis.
+                        </p>
+                    </div>
+                </div>
+            </div>
+*/
